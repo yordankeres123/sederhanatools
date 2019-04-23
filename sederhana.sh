15f3ed240296333f352dc1e2aa98802d4779f97f
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-04-23 19:25:14 +0700

    Add files via upload
    Cyb3r Pop1ce

---
+++ sederhana.sh
@@ -0,0 +1,74 @@
+#!/system/xbin/bash
+#mau ngapain lo cok!!
+#ngerecode/reedit
+#mikir goblok ga punya otak lo?
+#gue bikin susah eh malah direcode
+#ga bakal jadi mastah goblok
+#mikir aja
+#pm gue kalau mau ngerecode goblo
+#ni nomor wa ku 
+#6282233001174
+#pm aja saya baik bro :v
+#udah lah cok kluar aja
+#gabakal jadi mastah kalau ngerecode kontol
+clear
+figlet BIGBOS STARLA | lolcat
+
+echo "☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆" | lolcat
+echo "Team ;Cyb3r Pop1ce +62 " | lolcat
+echo "Project ; Bigbos Starla" | lolcat
+echo "Friends ; nofirends :(" | lolcat
+echo "Tools Sederhana" | lolcat
+echo "☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆" | lolcat
+echo
+echo
+echo "======================================="
+echo "kontak me" | lolcat
+echo "fb : Bigbos Starla" | lolcat
+echo "wa ; pilih 2 pasti kluar wanya" | lolcat
+echo "pless subs chanel ane" | lolcat
+echo "chanel Bigbos Starla" | lolcat
+echo "========================================"
+echo
+echo
+echo "♤♡♧♧♢♡♤♡♧♧♢♡♤♡♢♧♢♡♤♡♧♧♢♤♤♢♧♧♢♤♡♧♧♡♤♢♧♧♡♢" | lolcat
+echo "silahkan tekan 1 (enter)"
+echo
+echo "¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤"| lolcat
+echo
+echo "1. Tools  Sk4ters" | lolcat
+echo
+echo "2. EXIT " | lolcat
+echo
+echo "○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○" | lolcat
+echo
+read -p "By B16b05#" starla
+if [ $starla = 1 ] || [ $starla = 1 ]
+then
+clear
+figlet BGS.STLA | lolcat
+echo "Tunngu Broee"
+echo "LOADING 99%"
+sleep 1
+pkg install git
+git clone https://github.com/Sk4ters38/SK4T3R5v1
+cd SK4T3R5v1
+sh SK4T3RS.sh
+fi
+
+if [ $starla = 2 ] || [ $starla = 2 ]
+then
+echo "MAKASIH TELAH MEMAKAI TOOLS INI"
+sleep 1
+echo "KAMI KLUARGA CYB3R POP1CE +62" | lolcat
+sleep 1
+echo "jika ingin bergabung ke grup"
+sleep 1
+echo "silahkan hubungi admin nomor" | lolcat
+sleep 1
+echo "+6282233001174"
+sleep 1
+echo "penulis ;Bigbos Starla" | lolcat
+sleep 1
+exit
+fi
